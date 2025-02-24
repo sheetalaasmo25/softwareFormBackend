e18846af3eececb675d0499d03dc02f90259ee42
sheetalaasmo25 <your_email@example.com>
2025-02-24 17:44:58 +0530

    done

--- db/db.js
+++ db/db.js
@@ -1,16 +1,18 @@
+require('dotenv').config(); // Server.js ya index.js ke shuruat mein
+
 const mongoose = require('mongoose');
 
 // MongoDB connection function
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/pdfdb', {
+        await mongoose.connect(process.env.MONGO_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
         console.log('MongoDB connected');
     } catch (error) {
         console.error('Error connecting to MongoDB', error);
-        process.exit(1); // Exit the process if unable to connect
+        process.exit(1);
     }
 };
 
